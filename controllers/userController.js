83cb6ffe0acb0927622383d31e649d067d536713
Bhawani1200 <bhawanilimbu1200@gmail.com>
2024-06-12 14:04:52 +0545

    new added

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,6 +15,14 @@ const loginUser = async (req, res) => {
     res.status(404).send(error.message);
   }
 };
+const logOutUser=async(req,res)=>{
+  try {
+    res.clearCookie("cookie")
+    res.json("Okay")
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
+}
 const forgetPassword = async (req, res) => {
   try {
     const { email } = req.body;
@@ -28,5 +36,5 @@ export default {
   registerUser,
   loginUser,
   forgetPassword,
- 
+ logOutUser
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,7 @@ import userController from '../controllers/userController.js';
 const router=express.Router();
 router.post('/register',userController.registerUser)
 router.post('/login',userController.loginUser)
+router.post('/logout',userController.logOutUser)
 router.post('/forgetPassword',userController.forgetPassword)
 
 
