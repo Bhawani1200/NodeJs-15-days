03fe80ae2fba2ba13c0a8dd74fb72c1d7bd17e9f
Bhawani1200 <bhawanilimbu1200@gmail.com>
2024-06-18 18:44:33 +0545

    changes made

--- app.js
+++ app.js
@@ -1,30 +1,41 @@
 import express from 'express';
-import cors from 'cors'
+import dotenv from 'dotenv'
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import productRoutes from './routes/productRoutes.js'
-import userRoutes from './routes/userRoutes.js'
+// import userRoutes from './routes/userRoutes.js'
 import authRoutes from './routes/authRoutes.js'
 import businessRoutes from './routes/businessRoutes.js'
 import adminRoutes from './routes/adminRoutes.js'
 import bodyParser from 'body-parser';
 import connectDB from './database.js';
 import logger from './middleware/logger.js';
-import cookieParser from 'cookie-parser';
 const app=express();
 connectDB();
+dotenv.config();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(logger)
 app.use(cookieParser())
-app.use(cors({
-    origin:"http://localhost:3000"
-}))
+app.use(
+    cors({
+      origin: "http://localhost:3000",
+    })
+  );
 app.set('view engine','ejs')
 const PORT=process.env.PORT||5000;
 app.use("/api/products",productRoutes)
 app.use("/api/auth",authRoutes)
-app.use("/api/users",userRoutes)
+// app.use("/api/users",userRoutes)
 app.use("/api/business",businessRoutes)
 app.use("/api/admin",adminRoutes)
+app.get("/",(req,res)=>{
+    res.json({
+        appName:"CODE-IT Node js",
+        version:"1.1.0",
+        port:PORT
+    })
+})
 app.listen(PORT,()=>{
     console.log(`The port is running on ${PORT}`)
 })
